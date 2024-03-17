30dc9706d1bde9830e4a5f15a14e5df9d2c624ea
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-17 12:52:36 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,17 @@
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function (n) {
+
+    const NO_PIVOT_INTEGER_FOOUND = -1;
+    let sumAllValues = n * (n + 1) / 2;
+    let squareRootOfSumAllValuesRoundedDownToNearestInteger = Math.floor(Math.sqrt(sumAllValues));
+
+    return (squareRootOfSumAllValuesRoundedDownToNearestInteger
+            * squareRootOfSumAllValuesRoundedDownToNearestInteger)
+            === sumAllValues
+            ? squareRootOfSumAllValuesRoundedDownToNearestInteger
+            : NO_PIVOT_INTEGER_FOOUND;
+};

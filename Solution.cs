6d2d51e2df9585cb27c81854727f0fcb6feca53e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-17 12:55:42 +0100

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+public class Solution
+{
+    private static readonly int NO_PIVOT_INTEGER_FOOUND = -1;
+
+    public int PivotInteger(int n)
+    {
+
+        int sumAllValues = n * (n + 1) / 2;
+        int squareRootOfSumAllValuesRoundedDownToNearestInteger = (int)Math.Sqrt(sumAllValues);
+
+        return (squareRootOfSumAllValuesRoundedDownToNearestInteger
+                * squareRootOfSumAllValuesRoundedDownToNearestInteger)
+                == sumAllValues
+                ? squareRootOfSumAllValuesRoundedDownToNearestInteger
+                : NO_PIVOT_INTEGER_FOOUND;
+    }
+}

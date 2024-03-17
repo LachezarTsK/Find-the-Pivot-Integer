100374e305880128afb580ac5f046a002719f578
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-17 12:54:18 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,20 @@
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+
+    static const int NO_PIVOT_INTEGER_FOOUND = -1;
+
+public:
+    int pivotInteger(int n) {
+        int sumAllValues = n * (n + 1) / 2;
+        int squareRootOfSumAllValuesRoundedDownToNearestInteger = sqrt(sumAllValues);
+
+        return (squareRootOfSumAllValuesRoundedDownToNearestInteger
+                * squareRootOfSumAllValuesRoundedDownToNearestInteger)
+                == sumAllValues
+                ? squareRootOfSumAllValuesRoundedDownToNearestInteger
+                : NO_PIVOT_INTEGER_FOOUND;
+    }
+};
